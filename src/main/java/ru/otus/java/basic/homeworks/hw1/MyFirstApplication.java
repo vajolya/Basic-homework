1bb341cd4155a448eaf2ee414333c56241fdadae
Olga <vajolya@yandex.ru>
2024-05-11 14:27:58 +0300

    initial commit

---
+++ src/main/java/ru/otus/java/basic/homeworks/hw1/MyFirstApplication.java
@@ -0,0 +1,60 @@
+package ru.otus.java.basic.homeworks.hw1;
+
+ public class MyFirstApplication {
+    public static void main(String[] args) {
+    }
+
+        System.out.println("Hello");
+        System.out.println("Word");
+        System.out.println("from");
+        System.out.println("java");
+
+
+    }
+
+
+}
+
+   public static void checkSing(int a, int b, int c){
+
+    int a = 1;
+    int b= 5;
+    int c =10;
+    int sum (a+b+c);
+     if ( sum >= 0) {
+         System.out.println("Сумма положительная");
+     }
+       if (sum <= 0) {
+           System.out.println("Сумма отрицательная");
+       }
+
+
+}
+
+   public static void selectColor(int data ){
+    int data =5;
+
+    if (data < 10) {
+        System.out.println("Красный");
+    }
+       if (data < 20){
+           System.out.println("Желтый");
+       }
+       if (data < 25) {
+           System.out.println("Зеленый");
+       }
+
+}
+public static void compareNumbers (int a, int b){
+    int a =5;
+    int b = 8;
+    if (a <= b) {
+        System.out.println("a<=b");
+    } else {
+
+    }
+        System.out.println("a>=b");
+    }
+}
+
+
